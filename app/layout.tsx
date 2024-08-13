4bae9b8e058ec9e671d6e1d2a1a7816af6cd06cf
MrPolymath <danielcarmona55@gmail.com>
2024-08-13 15:03:02 +0200

    Add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-screen">
+      <GoogleTagManager gtmId="G-FNSTD4R21X" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
